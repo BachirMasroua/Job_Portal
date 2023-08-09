61df2761d60ec3c561524cf2fa45ab605e43b60b
Bachir Masroua <bachir.masroua@st.ul.edu.lb>
2023-08-09 11:12:22 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor:{
+        'custom-green': '#00CC99',
+      },
+      textColor: {
+        'custom-green': '#00CC99',
+      },
     },
   },
   plugins: [],
